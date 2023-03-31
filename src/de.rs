b729510046b20dca7e05c35181faa16a6c71b643
Ingvar Stepanyan <me@rreverser.com>
2023-03-31 12:21:49 +0100

    Merge first_pass into shape

--- src/de.rs
+++ src/de.rs
@@ -11,23 +11,29 @@ macro_rules! forward_visitors {
 
 struct Context<T> {
     data: Vec<T>,
-    shape: Vec<usize>,
+    shape: Option<Box<[usize]>>,
     current_depth: usize,
-    first_pass: bool,
 }
 
 impl<'de, T: Deserialize<'de>> Context<T> {
     fn got_number<E: Error>(&mut self) -> Result<(), E> {
-        if self.shape.get(self.current_depth).is_some() {
-            // We've seen a sequence at this depth before, but got a number now.
-            return Err(E::invalid_type(
-                serde::de::Unexpected::Other("a single number"),
-                &"a sequence",
-            ));
+        match &self.shape {
+            Some(shape) => {
+                if self.current_depth < shape.len() {
+                    // We've seen a sequence at this depth before, but got a number now.
+                    return Err(E::invalid_type(
+                        serde::de::Unexpected::Other("a single number"),
+                        &"a sequence",
+                    ));
+                }
+            }
+            None => {
+                // Once we've seen a numeric value for the first time, this means we reached the innermost dimension.
+                // From now on, start collecting shape info.
+                // To start, allocate the dimension lenghs with placeholders.
+                self.shape = Some(vec![0; self.current_depth + 1].into_boxed_slice());
+            }
         }
-        // Once we've seen a numeric value for the first time, this means we reached the innermost dimension.
-        // Unmark the first pass flag so that from now on we start validating shapes.
-        self.first_pass = false;
         Ok(())
     }
 
@@ -54,30 +60,13 @@ impl<'de, T: Deserialize<'de>> Visitor<'de> for &mut Context<T> {
     }
 
     fn visit_seq<A: SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
-        // The code paths for when we know the shape and when we don't are quite different,
-        // so we split them into two separate branches.
-        if self.first_pass {
-            // We're still in the first pass, so we don't know the shape yet.
-            debug_assert_eq!(self.current_depth, self.shape.len());
-            // Just push a `0` placeholder so that we could recurse deeper.
-            // It will be replaced with an actual length once we finish this
-            // sequence for the first time.
-            self.shape.push(0);
-            self.current_depth += 1;
-            // Consume & count all the elements.
-            let mut len = 0;
-            while seq.next_element_seed(&mut *self)?.is_some() {
-                len += 1;
-            }
-            self.current_depth -= 1;
-            // Replace the placeholder with the actual length.
-            debug_assert_eq!(self.shape[self.current_depth], 0);
-            self.shape[self.current_depth] = len;
-        } else {
+        // The code paths for when we know the shape and when we're still doing the first
+        // descent are quite different, so we split them into two separate branches.
+        if let Some(shape) = &self.shape {
             // This is not the first pass anymore, so we've seen all the dimensions.
             // Check that the current dimension has seen a sequence before and return
             // its expected length.
-            let expected_len = self.shape.get(self.current_depth).copied().ok_or_else(|| {
+            let expected_len = shape.get(self.current_depth).copied().ok_or_else(|| {
                 Error::invalid_type(serde::de::Unexpected::Seq, &"a single number")
             })?;
             // Consume the expected number of elements.
@@ -91,6 +80,22 @@ impl<'de, T: Deserialize<'de>> Visitor<'de> for &mut Context<T> {
                 return Err(Error::custom("expected end of sequence"));
             }
             self.current_depth -= 1;
+        } else {
+            // We're still in the first pass, so we don't know the shape yet.
+            debug_assert_eq!(self.shape, None);
+            self.current_depth += 1;
+            // Consume & count all the elements.
+            let mut len = 0;
+            while seq.next_element_seed(&mut *self)?.is_some() {
+                len += 1;
+            }
+            self.current_depth -= 1;
+            // Replace the placeholder `0` with the actual length.
+            let shape = self
+                .shape
+                .as_mut()
+                .expect("internal error: shape should be allocated by now");
+            shape[self.current_depth] = len;
         }
         Ok(())
     }
@@ -134,7 +139,7 @@ impl<'de, T: Deserialize<'de>> DeserializeSeed<'de> for &mut Context<T> {
 pub trait MakeNDim {
     type Item;
 
-    fn from_shape_and_data(shape: Vec<usize>, data: Vec<Self::Item>) -> Self;
+    fn from_shape_and_data(shape: Box<[usize]>, data: Vec<Self::Item>) -> Self;
 }
 
 pub fn deserialize<'de, T, D>(deserializer: D) -> Result<T, D::Error>
@@ -145,10 +150,9 @@ where
 {
     let mut context = Context {
         data: Vec::new(),
-        shape: Vec::new(),
+        shape: None,
         current_depth: 0,
-        first_pass: true,
     };
     deserializer.deserialize_any(&mut context)?;
-    Ok(T::from_shape_and_data(context.shape, context.data))
+    Ok(T::from_shape_and_data(context.shape.unwrap(), context.data))
 }
--- src/ndarray.rs
+++ src/ndarray.rs
@@ -3,7 +3,7 @@ use ndarray::{ArrayD, ShapeBuilder};
 impl<T> super::de::MakeNDim for ArrayD<T> {
     type Item = T;
 
-    fn from_shape_and_data(shape: Vec<usize>, data: Vec<Self::Item>) -> Self {
+    fn from_shape_and_data(shape: Box<[usize]>, data: Vec<Self::Item>) -> Self {
         ArrayD::from_shape_vec(shape.into_shape(), data)
             .expect("internal mismatch: parsed shape doesn't match the parsed data")
     }
