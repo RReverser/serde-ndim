0c742d7f082a65e3bdd994e1ad3222c74a921aef
Ingvar Stepanyan <me@rreverser.com>
2023-04-02 15:34:56 +0100

    Fix unused import

--- src/de.rs
+++ src/de.rs
@@ -1,4 +1,3 @@
-use core::iter;
 use serde::de::{DeserializeSeed, Error, IgnoredAny, IntoDeserializer, SeqAccess, Visitor};
 use serde::{Deserialize, Deserializer};
 use std::borrow::BorrowMut;
@@ -64,7 +63,7 @@ impl<const MAX_DIMS: usize> Shape for arrayvec::ArrayVec<usize, MAX_DIMS> {
     fn new_zeroed(dims: usize) -> Self {
         debug_assert!(dims <= MAX_DIMS);
         let mut shape = Self::new();
-        shape.extend(iter::repeat(0).take(dims));
+        shape.extend(core::iter::repeat(0).take(dims));
         shape
     }
 }
